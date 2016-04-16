b81544debd82517885d6dfaad8788008dfa7914c
ginefrides <aluno@ifceadmin-IPMH61P1.(none)>
2016-04-16 11:34:34 -0300

    adicionando identificador de pares

---
+++ par.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int numero;
+
+	printf("Digite um numero: ");
+	scanf("%d" ,&numero);
+
+	if(numero % 2 == 0)
+	printf("0 %d é par!", numero);
+
+}
